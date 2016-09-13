ea9691cec8e2f0427d127cea65685c3b4f0adf86
Ryan Travitz <rtravitz@gmail.com>
2016-09-12 22:22:00 -0600

    added iteration 0 find and load for enrollment repo

--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -1,3 +1,26 @@
+require 'csv'
+require './lib/enrollment'
+
 class EnrollmentRepository
-  
+  attr_reader :repository
+
+  def initialize
+    @repository = Hash.new
+  end
+
+  def load_data(path)
+    CSV.foreach path[:enrollment][:kindergarten], headers: true, header_converters: :symbol do |row|
+      unless location_exists?(row[:location])
+        @repository[row[:location].upcase] = Enrollment.new({name: row[:location].upcase, kindergarten_participation: row[:data]})
+      end
+    end
+  end
+
+  def location_exists?(location)
+    @repository[location.upcase] ? true : false
+  end
+
+  def find_by_name(name)
+    @repository[name]
+  end
 end
--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -8,4 +8,28 @@ class EnrollmentRepositoryTest < Minitest::Test
 
     assert_instance_of EnrollmentRepository, er
   end
+
+  def test_load_data_creates_hash_of_enrollments
+    er = EnrollmentRepository.new
+    er.load_data({
+      :enrollment => {
+      :kindergarten => "./data/Kindergartners in full-day program.csv"
+      }
+    })
+
+    er.repository.each do |name, enrollment|
+      assert_instance_of Enrollment, enrollment
+    end
+  end
+
+  def test_find_by_name_returns_correct_enrollment
+    er = EnrollmentRepository.new
+    er.load_data({
+      :enrollment => {
+      :kindergarten => "./data/Kindergartners in full-day program.csv"
+      }
+    })
+    enrollment = er.find_by_name("ACADEMY 20")
+    assert_equal "ACADEMY 20", enrollment.name
+  end
 end
