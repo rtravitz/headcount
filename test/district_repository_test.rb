d0efff56b7fb27fcdc7f0d6829215df37425a6a2
Ryan Travitz <rtravitz@gmail.com>
2016-09-16 20:00:06 -0600

    finalized iteration three minus errors

--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -63,7 +63,29 @@ class DistrictRepositoryTest < Minitest::Test
    district = dr.find_all_matching("Adam")
    assert_equal 2, district.count
    assert_equal [], dr.find_all_matching("blahhdhdhfd")
- end
+  end
+
+  def test_district_repo_loads_multiple_datasets
+    dr = DistrictRepository.new
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv",
+        :high_school_graduation => "./data/High school graduation rates.csv",
+      },
+      :statewide_testing => {
+        :third_grade => "./data/3rd grade students scoring proficient or above on the CSAP_TCAP.csv",
+        :eighth_grade => "./data/8th grade students scoring proficient or above on the CSAP_TCAP.csv",
+        :math => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Math.csv",
+        :reading => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv",
+        :writing => "./data/Average proficiency on the CSAP_TCAP by race_ethnicity_ Writing.csv"
+      }
+    })
+
+    district = dr.find_by_name("ACADEMY 20")
+
+    assert_instance_of Enrollment, district.enrollment
+    assert_instance_of StatewideTest, district.statewide_test
+  end
 
 
 end
