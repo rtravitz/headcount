5f3de5999317be4300815a70f103d5198ea6875f
Ryan Travitz <rtravitz@gmail.com>
2016-09-21 22:44:28 -0600

    added more for economic profile

--- test/economic_profile_test.rb
+++ test/economic_profile_test.rb
@@ -37,6 +37,12 @@ class EconomicProfileTest < Minitest::Test
     end
   end
 
+  def test_children_in_poverty_average
+    ep = EconomicProfile.new(data)
+
+    assert_in_delta 0.159, ep.children_in_poverty_average, 0.005
+  end
+
   def test_free_or_reduced_lunch_percentage_in_year
     ep = EconomicProfile.new(data)
 
@@ -55,6 +61,12 @@ class EconomicProfileTest < Minitest::Test
     end
   end
 
+  def test_free_or_reduced_price_lunch_average
+    ep = EconomicProfile.new(:free_or_reduced_price_lunch => {2014 => {:percent => 0.023, :total => 100}, 2015 => {percent: 0.123, total: 50}})
+
+    assert_equal 0.073, ep.free_or_reduced_price_lunch_average, 0.005
+  end
+
   def test_title_i_in_year
     ep = EconomicProfile.new(data)
 
@@ -64,10 +76,16 @@ class EconomicProfileTest < Minitest::Test
     end
   end
 
+  def test_find_all_median_years
+    ep = EconomicProfile.new(data)
+
+    assert_equal [2013, 2014, 2015], ep.find_all_median_years.sort
+  end
+
   def data
   { :median_household_income => {[2014, 2015] => 50000, [2013, 2014] => 60000},
-    :children_in_poverty => {2012 => 0.1845},
-    :free_or_reduced_price_lunch => {2014 => {:percentage => 0.023, :total => 100}},
+    :children_in_poverty => {2012 => 0.1845, 2013 => 0.1325},
+    :free_or_reduced_price_lunch => {2014 => {:percentage => 0.023, :total => 100}, 2015 => {percentage: 0.123, total: 50}},
     :title_i => {2015 => 0.543},
    }
   end
