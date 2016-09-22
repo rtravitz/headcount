def6f866a36e1b9495b7b0067b06e7f76b401009
Ryan Travitz <rtravitz@gmail.com>
2016-09-21 23:05:51 -0600

    added more for organizer

--- test/organizer_test.rb
+++ test/organizer_test.rb
@@ -32,6 +32,30 @@ class OrganizerTest < Minitest::Test
     assert_equal expected, action
   end
 
+  def test_organize_median_income
+    action = Organizer.organize_median_income(economic_data)
+
+    assert_equal ({["2005", "2009"]=>56222.0}), action
+  end
+
+  def test_organize_children_in_poverty
+    action = Organizer.organize_children_in_poverty(economic_data)
+
+    assert_equal ({1995 => 0.032}), action
+  end
+
+  def test_organize_lunch
+    action = Organizer.organize_lunch(economic_data)
+
+    assert_equal ({2000=>{:percent=>0.27}}), action
+  end
+
+  def test_organize_title_i
+    action = Organizer.organize_title_i(economic_data)
+
+    assert_equal ({2009=>0.216}), action
+  end
+
   def enrollment_data
     [{ :location=>"COLORADO", :timeframe=>2007, :dataformat=>"Percent",
        :data=>0.395, :source=>:kindergarten},
@@ -49,6 +73,15 @@ class OrganizerTest < Minitest::Test
       {:location=>"COLORADO", :race_ethnicity=>"White", :source=>:math},
       {:location=>"COLORADO", :score=>"Reading", :source=>:eighth_grade}
     ]
+  end
+
+  def economic_data
+    {
+      median_household_income: [{:location=>"COLORADO", :timeframe=>"2005-2009", :dataformat=>"Currency", :data=>56222, :source=>:median_household_income}],
+      free_or_reduced_price_lunch: [{:location=>"COLORADO", :poverty_level=>"Eligible for Free or Reduced Lunch", :timeframe=>2000, :dataformat=>"Percent", :data=>0.27, :source=>:free_or_reduced_price_lunch}],
+      title_i: [{:location=>"COLORADO", :timeframe=>2009, :dataformat=>"Percent", :data=>0.216, :source=>:title_i}],
+      children_in_poverty: [{:location=>"ACADEMY 20", :timeframe=>1995, :dataformat=>"Percent", :data=>0.032, :source=>:children_in_poverty}]
+    }
   end
 
 end
